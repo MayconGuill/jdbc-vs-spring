124af259d25bd1fccffb625f097be5bd5ef4e174
MayconGuill <mayconguill@gmail.com>
2025-09-05 12:44:46 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,16 @@ package application;
 
 import java.util.Date;
 
+import domain.dao.DaoFactory;
+import domain.dao.SellerDao;
 import domain.entities.Department;
 import domain.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Seller obj = new Seller(1, "Books", "books@gmail.com", new Date(), 2000.00, new Department(1, "Books"));
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 	}
-
 }
--- src/domain/dao/DaoFactory.java
+++ src/domain/dao/DaoFactory.java
@@ -1,8 +1,11 @@
 package domain.dao;
 
+import db.DB;
+import domain.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
