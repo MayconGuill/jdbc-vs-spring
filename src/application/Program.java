04b12d36eb9582acf0518ffc5ae8e97e29a6ac08
MayconGuill <mayconguill@gmail.com>
2025-09-05 10:12:37 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import java.util.Date;
+
 import domain.entities.Department;
+import domain.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
+		Seller obj = new Seller(1, "Books", "books@gmail.com", new Date(), 2000.00, new Department(1, "Books"));
 		System.out.println(obj);
 	}
 
