8efb169c08bf5ab00a3c67fd0ff7a7d8179b1609
MayconGuill <mayconguill@gmail.com>
2025-09-05 10:21:22 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/domain/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package domain.dao;
+
+import java.util.List;
+
+import domain.entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+}
